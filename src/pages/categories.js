3ceede929f1a62930c25cf5f02dfe5f636fd200a
Flukekie <git@flukekie.me>
2019-09-20 03:15:48 +0700

    initial commit: basically everything for v0.1
    added sitemap plugin
    
    
    quick test on netlify
    
    swaparoo

---
+++ src/pages/categories.js
@@ -0,0 +1,52 @@
+import React from "react"
+
+// Utilities
+import kebabCase from "lodash/kebabCase"
+
+// Components
+import { Helmet } from "react-helmet"
+import { Link, graphql } from "gatsby"
+
+const TagsPage = ({
+  data: {
+    allMarkdownRemark: { group },
+    site: {
+      siteMetadata: { title },
+    },
+  },
+}) => (
+  <div>
+    <Helmet title={title} />
+    <div>
+      <h1>Categories</h1>
+      <ul>
+        {group.map(cat => (
+          <li key={cat.fieldValue}>
+            <Link to={`/category/${kebabCase(cat.fieldValue)}/`}>
+              {cat.fieldValue} ({cat.totalCount})
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+)
+
+
+export default TagsPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(limit: 2000) {
+      group(field: frontmatter___categories) {
+        fieldValue
+        totalCount
+      }
+    }
+  }
+`
